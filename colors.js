dca4ba14bda28b133eb3b78065e5e85601ca9bad
MeritKoch <mkoch41@smail.uni-koeln.de>
2024-05-02 17:02:16 +0200

    Color JS

---
+++ colors.js
@@ -0,0 +1,9 @@
+const COLORS = {
+    temperature: [
+        { min: -50, max: -25, color: "#9f80ff" },
+        { min: -25, max: -20, color: "##784cff" },
+        { min: -20, max: -15, color: "#9f80ff" },
+        { min: -15, max: -10, color: "#9f80ff" },
+        { min: -10, max: -5, color: "#9f80ff" },
+    ]
+};
